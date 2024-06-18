fd34f4200963d1d8d08f43eba013a070b6a54de6
Nandhagopiv <nandhagopy@gmail.com>
2024-06-18 19:23:41 +0530

    Added:Edit func in todo component

--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react"
 
 function TodoBox() {
-    const [userList, setuserList] = useState([])
-
+    const [userList, setuserList] = useState([
+        {
+            id:1,
+            task:"apple"
+        },
+        {
+            id:2,
+            task:"orange"
+        },
+        {
+            id:3,
+            task:"grape"
+        },
+        {
+            id:4,
+            task:"banana"
+        },
+    ])
+    const [modify, setModify] = useState(false)
     const [inputVal, setinputVal] = useState("")
+    const [tempArrf,setTempArrf] = useState([])
+    const [tempArrl,setTempArrl] = useState([])
+    const [modifybtn,setModifybtn] = useState(false)
 
     function handleDelete(id) {
 
@@ -19,12 +39,35 @@ function TodoBox() {
     }
 
     function handleAdd() {
+        console.log(tempArrf,tempArrl);
         if (inputVal === "") {
             alert("Enter something")
         } else {
-            setuserList([...userList, { id: userList.length + 1, task: inputVal }])
-            setinputVal("")
+            if (modify === true) {
+                setuserList([...tempArrf,{id:tempArrf.length + 1,task:inputVal},...tempArrl])
+                setModify(false)
+                setinputVal("")
+                setModifybtn(false)
+            } else {
+                setuserList([...userList, { id: userList.length + 1, task: inputVal }])
+                setinputVal("")
+            }
         }
+    }
+
+    function handleEdit(id) {
+        setModifybtn(true)
+        var clickedTodo = userList.filter((data) => {
+            if (data.id === id) {
+                return true
+            } else {
+                return false
+            }
+        })
+        setModify(true)
+        setinputVal(clickedTodo[0].task)
+        setTempArrf(userList.slice(0,id-1))
+        setTempArrl(userList.slice(id,userList.length))
     }
 
     function handleChange(e) {
@@ -36,7 +79,7 @@ function TodoBox() {
             <div className="flex flex-col">
                 <h1 className="text-3xl font-bold">Manage Activities</h1>
                 <input value={inputVal} onChange={handleChange} className="p-2 border border-none rounded-md bg-slate-300 outline-none my-3" placeholder="Enter your Activity" type="text"></input>
-                <button onClick={handleAdd} className="p-2 border border-none rounded-md text-white px-5 bg-black">Add</button>
+                <button onClick={handleAdd} className="p-2 border border-none rounded-md text-white px-5 bg-black">{modifybtn?"Modify":"Add"}</button>
             </div>
             <div className="bg-[#BDB4EA] p-3 border rounded-xl flex-grow">
                 <h1 className="text-2xl font-bold ">Today's Activity</h1>
@@ -46,7 +89,7 @@ function TodoBox() {
                 {
                     userList.map((data, index) => {
                         return <div className="flex justify-between">
-                            <p style={{wordBreak:"break-word"}} className="py-3 text-xl">{index + 1}. {data.task}</p> <button onClick={() => handleDelete(data.id)} className=" bg-black m-2 p-1 border h-10 border-none rounded-md text-white">Delete</button>
+                            <p style={{ wordBreak: "break-word" }} className="py-3 text-xl">{index + 1}. {data.task}</p> <div><button onClick={() => handleEdit(data.id)} className=" bg-black m-2 p-1 border h-10 border-none rounded-md text-white">Edit</button> <button onClick={() => handleDelete(data.id)} className=" bg-black m-2 p-1 border h-10 border-none rounded-md text-white">Delete</button></div>
                         </div>
                     })
                 }
