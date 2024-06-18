028876e13653828c92d2fcee59857db893d4dd88
Nandhagopiv <nandhagopy@gmail.com>
2024-06-18 17:32:08 +0530

    Added Login and Signup func

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,50 +4,57 @@ import { useNavigate } from "react-router-dom"
 
 function Login(props) {
 
-    const[eUsername,seteUsername] = useState()
-    const[ePassword,setePassword] = useState()
-    const[verifyUser,setverifyUser] = useState(true)
+    const [eUsername, seteUsername] = useState('')
+    const [ePassword, setePassword] = useState('')
+    const[msgCol,setmsgCol] = useState(false)
+    const [msg, setmsg] = useState("I help you manage your daily activities:)")
     const Navigate = useNavigate()
 
-    function handleUsername(e){
+    function handleUsername(e) {
         seteUsername(e.target.value)
     }
 
-    function handlePassword(e){
+    function handlePassword(e) {
         setePassword(e.target.value)
     }
 
-    function userCheck(){
+    function userCheck(e) {
+        e.preventDefault()
         let userfound = false
-        props.users.forEach((data)=>{
+        
+        props.users.forEach((data) => {
             if (eUsername === data.username && ePassword === data.password) {
-                setverifyUser(true)
+                Navigate("/home", { state: { user: eUsername } })
                 userfound = true
-                Navigate("/home",{state:{user:eUsername}})
+            } else if (eUsername === data.username && ePassword !== data.password) {
+                setmsg("Incorrect Password")
+                setmsgCol(true)
+                userfound = true
             }
 
             if (userfound === false) {
-                setverifyUser(false)
+                setmsg("User not Found. Sign up and Login Again")
+                setmsgCol(true)
             }
         })
     }
 
     return (
-    <div className="flex justify-center my-40">
-        
-        <div className="flex flex-col gap-4 m-10 w-3/4 md:w-4/12">
-            <h1 className="text-3xl font-bold">Hey Hello</h1>
-            {
-                verifyUser? <p className="text-xl">I help you manage your daily activities:)</p> : <p className="text-xl text-red-600">You are a New User. Please Signup before you Login</p>
-            }
-            <div className="flex gap-4 flex-col">
-                <input value={eUsername} onChange={handleUsername} className="p-2 outline-none border bg-slate-200 rounded-md border-none" placeholder="username"></input>
-                <input value={ePassword} onChange={handlePassword} className="p-2 outline-none border bg-slate-200 rounded-md border-none" placeholder="password"></input>
-                <button onClick={userCheck} className="p-2 bg-purple-950 text-white font-bold border rounded-md border-none">Login</button>
+        <div className="flex justify-center my-40">
+
+            <div className="flex flex-col gap-4 m-10 w-3/4 md:w-4/12">
+                <h1 className="text-3xl font-bold">Hey Hello</h1>
+                {
+                    <p style={{color: msgCol?"red":"black"}} className="text-xl">{msg}</p>
+                }
+                <form onSubmit={userCheck} className="flex gap-4 flex-col">
+                    <input required type="text" value={eUsername} onChange={handleUsername} className="p-2 outline-none border bg-slate-200 rounded-md border-none" placeholder="username"></input>
+                    <input required type="password" value={ePassword} onChange={handlePassword} className="p-2 outline-none border bg-slate-200 rounded-md border-none" placeholder="password"></input>
+                    <button type="submit" className="p-2 bg-purple-950 text-white font-bold border rounded-md border-none">Login</button>
+                </form>
+                <p>Don't have an account? <Link to={"/signup"} className="underline">Signup</Link></p>
             </div>
-            <p>Don't have an account? <Link to={"/signup"} className="underline">Signup</Link></p>
         </div>
-    </div>
     )
 }
 
--- src/components/Signing.jsx
+++ src/components/Signing.jsx
@@ -3,21 +3,60 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 function Signing(props) {
-    const[eUsername,seteUsername] = useState()
-    const[ePassword,setePassword] = useState()
+    const [eUsername, seteUsername] = useState('')
+    const [ePassword, setePassword] = useState('')
+    const [eCrtPassword, seteCrtPassword] = useState('')
+    const [msg, setmsg] = useState("You can sign up here:)")
+    const [msgCol, setmsgCol] = useState(false)
+    const [userFound,setUserFound] = useState(false)
     const Navigate = useNavigate()
 
-    function handleUsername(e){
+    function handleUsername(e) {
         seteUsername(e.target.value)
     }
 
-    function handlePassword(e){
+    function checkOldUser() {
+        let userfound = false
+        props.users.forEach((data) => {
+            if (data.username === eUsername) {
+                setUserFound(true)
+                userfound=true
+            } else {
+                setmsg("You can sign up here:)")
+                setmsgCol(false)
+                setUserFound(false)
+            }
+
+            if (userfound === true) {
+                setmsg("User already Existed")
+                setmsgCol(true)
+            }
+        })
+    }
+
+    function handlePassword(e) {
         setePassword(e.target.value)
     }
 
-    function addUsers(){
-        props.setUsers([...props.users,{username:eUsername,password:ePassword}])
-        Navigate('/')
+    function handleCrtPassword(e) {
+        seteCrtPassword(e.target.value)
+    }
+
+    function addUsers(e) {
+        e.preventDefault()
+        if (userFound === true) {
+            alert("User already existed")
+        } else {
+            if (ePassword !== eCrtPassword) {
+                setmsg("Your Passwords are not Matching")
+                setmsgCol(true)
+            } else {
+                props.setUsers([...props.users, { username: eUsername, password: ePassword }])
+                Navigate('/')
+                alert("Sucessfully Created")
+                console.log(userFound);
+            }
+        }
     }
 
     return (
@@ -25,13 +64,13 @@ function Signing(props) {
             <div className="flex justify-center my-40">
                 <div className="flex flex-col gap-5 w-3/4 md:w-4/12">
                     <h1 className="text-3xl font-bold">Hey Hello</h1>
-                    <p className="text-xl">You can sign up here:)</p>
-                    <div className="flex gap-5 flex-col">
-                        <input value={eUsername} onChange={handleUsername} className="p-2 bg-slate-200 outline-none border rounded-md border-none" placeholder="username"></input>
-                        <input value={ePassword} onChange={handlePassword} className="p-2 bg-slate-200 outline-none border rounded-md border-none" placeholder="password"></input>
-                        <input className="p-2 bg-slate-200 outline-none border rounded-md border-none" placeholder="confirm password"></input>
-                        <button onClick={addUsers} className="p-2 bg-yellow-800 text-white font-bold border rounded-md border-none">Signup</button>
-                    </div>
+                    <p style={{ color: msgCol ? "red" : "black" }} className="text-xl">{msg}</p>
+                    <form onSubmit={addUsers} className="flex gap-5 flex-col">
+                        <input required type="text" value={eUsername} onKeyUp={checkOldUser} onChange={handleUsername} className="p-2 bg-slate-200 outline-none border rounded-md border-none" placeholder="username"></input>
+                        <input required type="password" value={ePassword} onChange={handlePassword} className="p-2 bg-slate-200 outline-none border rounded-md border-none" placeholder="password"></input>
+                        <input required type="password" value={eCrtPassword} onChange={handleCrtPassword} className="p-2 bg-slate-200 outline-none border rounded-md border-none" placeholder="confirm password"></input>
+                        <button type="submit" className="p-2 bg-yellow-800 text-white font-bold border rounded-md border-none">Signup</button>
+                    </form>
                     <p>Already have an account?<Link to={"/"} className="underline"> Login</Link></p>
                 </div>
             </div>
